c66c7969b91bbcf8b17091c3ef4a2dffc0fc44c2
shia <3122264539@qq.com>
2024-10-14 23:56:41 +0800

    初始化，重置css样式

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
+import "normalize.css"
+import "./assets/css/index.css"
 
-app.use(router)
-
-app.mount('#app')
+createApp(App).mount('#app')
