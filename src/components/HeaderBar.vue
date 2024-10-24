7ef606c980e6058245f6645d04ad5f66434338cd
zhuzirui <zhuzr3210@163.com>
2024-10-24 18:20:07 +0800

    修改首部导航栏路由信息

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -2,23 +2,29 @@
     <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :ellipsis="false"
         @select="handleSelect">
         <el-menu-item index="0">
-            <img style="width: 50px" src="../assets/images/logo.svg" alt="ZhuZiRui Logo" />
+            <img style="width: 50px" src="@/assets/images/logo.svg" alt="Home Logo" />
         </el-menu-item>
-        <RouterLink to="/about"><el-menu-item index="1">关于我们</el-menu-item></RouterLink>
+        <el-menu-item index="/about">关于我们</el-menu-item>
         <el-sub-menu index="3">
             <template #title>用户</template>
-            <RouterLink to="/users/personalpage"><el-menu-item index="2-1">用户中心</el-menu-item></RouterLink>
-            <RouterLink to="/logout"><el-menu-item index="2-2">登出</el-menu-item></RouterLink>
+            <el-menu-item index="/users/personalpage">用户中心</el-menu-item>
+            <el-menu-item index="/logout">登出</el-menu-item>
         </el-sub-menu>
     </el-menu>
 </template>
 
-<script lang="ts" setup>
-import { ref } from 'vue'
-
-const activeIndex = ref('1')
-const handleSelect = (key: string, keyPath: string[]) => {
-    console.log(key, keyPath)
+<script setup>
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router';
+const router = useRouter()
+const route = useRoute()
+const activeMenu = ref(route.path);
+watch(route, (newRoute) => {
+    activeMenu.value = newRoute.path;
+})
+const handleSelect = (key) => {
+    activeMenu.value
+    router.push(key)
 }
 </script>
 
