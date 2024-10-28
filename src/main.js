da0aa8a2132a3bd21a6fe4024349852bff146fe3
shia <3122264539@qq.com>
2024-10-28 22:24:01 +0800

    初始化我的题库

--- src/main.js
+++ src/main.js
@@ -5,7 +5,11 @@ import router from "./router";
 import { createApp } from "vue";
 import App from "./App.vue";
 import {createPinia} from 'pinia'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 const pinia=createPinia();
 app.use(pinia);
 app.use(router);
