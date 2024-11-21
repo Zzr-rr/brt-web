c79ebe7615dcde2b96828cd67f466e54e4801f18
shia <3122264539@qq.com>
2024-11-21 23:43:53 +0800

    尝试svg

--- src/main.js
+++ src/main.js
@@ -6,11 +6,13 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import {createPinia} from 'pinia'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import gsap from "gsap";
 const app = createApp(App);
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 const pinia=createPinia();
+app.use(gsap);
 app.use(pinia);
 app.use(router);
 app.use(ElementPlus);
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,59 @@
 <template>
-  <div>
-    <h1>This is an about page</h1>
+  <div class="boxcontain">
+    <div class="box box1"></div>
+  <div class="box box2"></div>
   </div>
+  
 </template>
-<script></script>
-<style></style>
+
+<script setup>
+import { gsap } from 'gsap';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  gsap.to(".box1", { 
+    x: window.innerWidth - 300,
+    duration: 3,
+    rotation: 360
+  });
+  gsap.to(".box2", { 
+    x: window.innerWidth - 300,
+    duration: 3.5,
+    rotation: 360
+  });
+});
+</script>
+
+<style scoped>
+body {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  min-height: 100vh;
+  margin: 0;
+  overflow: hidden; /* 禁止滚动 */
+}
+
+html {
+  overflow: hidden; /* 确保 HTML 也禁止滚动 */
+}
+.boxcontain{
+  
+}
+.box {
+  display: block;
+  background-color: black;
+  border-radius: 2px;
+  height: 20px;
+  width: 20px;
+  margin: 10px;
+}
+
+.box1 {
+  background-color: red; /* 如果需要，可以对 box1 定制样式 */
+}
+
+.box2 {
+  background-color: blue; /* 如果需要，可以对 box2 定制样式 */
+}
+</style>
