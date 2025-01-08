19e4c049e1152e0a6fcd311688564a1a8d3f89e2
666xz666 <2965962144@qq.com>
2025-01-08 16:27:08 +0800

    美化首页

--- src/api/personPage.js
+++ src/api/personPage.js
@@ -4,8 +4,8 @@ const personPageApi={
     info() {
         return axios.get('/brt/user/info');  // 假设这是获取文件列表的接口
     },
-    update(){
-        return axios.post('/brt/user/update');
+    update(params){
+        return axios.post('/brt/user/update',params);
     },
 };
 
